ce5ff45140d7c4b7c040a6c2645a6a671c65df9e
brad <bradmedeiros0@gmail.com>
2018-03-29 08:00:41 +0000

    symlink scripts into bin folder on install

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
 
+apt-get install git
 mkdir /opt/raspberry-pi-build-server
-cp scripts/* /opt/raspberry-pi-build-server
+
+scripts=( set-build-repo get-build-repo set-build-frequency  get-build-frequency last-build-time set-build-hook )
+
+
+for element in ${scripts[@]}
+do
+	cp "scripts/$element" /opt/raspberry-pi-build-server
+	ln -s "/opt/raspberry-pi-build-server/$element" "/usr/bin/$element"
+done
+
+
+
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
 rm -rf /opt/raspberry-pi-build-server
+
+scripts=( set-build-repo get-build-repo set-build-frequency  get-build-frequency last-build-time set-build-hook )
+
+
+for element in ${scripts[@]}
+do
+        rm "/usr/bin/$element"
+done
+
+
+
+
