a7e9c240e200cd4f987f89ba188b65f18e3002d6
brad <bradmedeiros0@gmail.com>
2018-03-30 08:46:31 +0000

    basic build script

---
+++ build_app.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+github_repo=$(get-build-repo)
+
+
+clone_directory_path=/opt/raspberry-pi-build-server/repo
+
+(
+	cd "$clone_directory_path"
+	git clone "$github_repo"
+        ./build-raspberry.sh
+)
--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 apt-get install git
 mkdir /opt/raspberry-pi-build-server
+mkdir /opt/raspberry-pi-build-server/scripts
+mkdir /opt/raspberry-pi-build-server/repo
+
 cp uninstall.sh /opt/raspberry-pi-build-server
 
 scripts=( set-build-repo get-build-repo set-build-frequency  get-build-frequency last-build-time set-build-hook )
@@ -9,10 +12,10 @@ scripts=( set-build-repo get-build-repo set-build-frequency  get-build-frequency
 
 for element in ${scripts[@]}
 do
-	cp "scripts/$element" /opt/raspberry-pi-build-server
-	ln -s "/opt/raspberry-pi-build-server/$element" "/usr/bin/$element"
+	cp "scripts/$element" /opt/raspberry-pi-build-server/scripts
+	ln -s "/opt/raspberry-pi-build-server/scripts/$element" "/usr/bin/$element"
 done
 
-
+cp -r data /opt/raspberry-pi-build-server
 
 
