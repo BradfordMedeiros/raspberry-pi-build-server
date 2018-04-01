c6d760716bd24b6d015983429cd3deee0db605ec
brad <bradmedeiros0@gmail.com>
2018-04-01 07:58:48 +0000

    -

--- build-app.sh
+++ build-app.sh
@@ -4,9 +4,9 @@ github_repo=$(get-build-repo)
 
 clone_directory_path=/opt/raspberry-pi-build-server/repo
 (
-	#git clone "$github_repo" "$clone_directory_path"
-        #cd "$clone_directory_path"
-	#./build-raspberry.sh
+	git clone "$github_repo" "$clone_directory_path"
+        cd "$clone_directory_path"
+	./build-raspberry.sh
 	build_hook_script=$(get-build-hook)
 	$build_hook_script
         rm -rf "$clone_directory_path"
